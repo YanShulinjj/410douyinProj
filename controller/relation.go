19dfe899aaf7edb341d69c2e175b786209199b73
fangjiaxin111 <1539289187@qq.com>
2022-06-11 17:11:34 +0800

    second commit

--- controller/relation.go
+++ controller/relation.go
@@ -9,30 +9,34 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-//var UserFollowMap = map[uint][]User{}
-//var UserFollowerMap = map[uint][]User{}
-//var UserFollowCountMap = map[uint]int64{}
-//var UserFollowerCountMap = map[uint]int64{}
+//var UserFollowMap = map[uint][]User{}       //用户关注的人
+//var UserFollowerMap = map[uint][]User{}     //该用户的粉丝
+//var UserFollowCountMap = map[uint]int64{}   //用户关注人的数量
+//var UserFollowerCountMap = map[uint]int64{} //用户粉丝的数量
 
 func init() {
 	initMaps()
 }
 
 func initMaps() {
-	var UserFollowCountMap = map[uint]int64{}
+	users := GetUsersBriefInfo()
 	var UserFollowMap = map[uint][]User{}
 	var UserFollowerMap = map[uint][]User{}
+	var UserFollowCountMap = map[uint]int64{}
 	var UserFollowerCountMap = map[uint]int64{}
-	users := GetUsersBriefInfo()
 	for _, user := range users {
 		// 解析follow
 		fuids := strings.Split(user.FollowID, ".")[1:]
+
 		for _, fuid := range fuids {
+
 			fid, err := strconv.Atoi(fuid)
+
 			if err != nil {
 				fmt.Println("*****Get video_id faild! ", err)
 				continue
 			}
+
 			follow, _ := FindUserByID(uint(fid))
 			UserFollowMap[user.ID] = append(UserFollowMap[user.ID], follow)
 			UserFollowerMap[follow.ID] = append(UserFollowerMap[follow.ID], user)
@@ -41,7 +45,7 @@ func initMaps() {
 		UserFollowerCountMap[user.ID] = user.FollowerCount
 	}
 	SaveUserFollowMap("UserFollowMap", Client, UserFollowMap)
-	SaveUserFollowerMap("UserFollowerMap", Client, UserFollowMap)
+	SaveUserFollowerMap("UserFollowerMap", Client, UserFollowerMap)
 	SaveUserFollowCountMap("UserFollowCountMap", Client, UserFollowCountMap)
 	SaveUserFollowerCountMap("UserFollowerCountMap", Client, UserFollowerCountMap)
 }
@@ -52,91 +56,148 @@ type UserListResponse struct {
 }
 
 // 点击关注
-// 将对方用户添加到自身列表
+
 func RelationAction(c *gin.Context) {
-	token := c.Query("token")
-	to_user_id, err := strconv.Atoi(c.Query("to_user_id"))
+
+	token := c.Query("token")                              //正在使用中的用户
+	to_user_id, err := strconv.Atoi(c.Query("to_user_id")) //发布视频的用户
+	action_type := c.Query("action_type")
+	UserFollowerMap := GetUserFollowerMap("UserFollowerMap", Client)
 	if err != nil {
 		fmt.Println("Get user_id faild! ", err)
 		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "get user_id faile !"})
 	}
-	UserFollowMap := GetUserFollowMap("UserFollowMap", Client)
-	UserFollowerMap := GetUserFollowMap("UserFollowerMap", Client)
-	UserFollowCountMap := GetUserFollowCountMap("UserFollowCountMap", Client)
-	UserFollowerCountMap := GetUserFollowerCountMap("UserFollowerCountMap", Client)
+	follow, _ := FindUserByID(uint(to_user_id))
 	if user, exist := FindUserInfo(token); exist {
-		follow, _ := FindUserByID(uint(to_user_id)) //找到关注对象的信息
-		// 如果还没关注
-		if !follow.IsFollow {
-			follow.IsFollow = true
-			// 更改缓冲区  对Map进行修改
-			UserFollowMap[user.ID] = append(UserFollowMap[user.ID], follow) //添加关注内容
-			UserFollowCountMap[user.ID]++
+		// 自己关注不了自己
+		if follow.Name == user.Name {
 
-			UserFollowerMap[uint(to_user_id)] = append(UserFollowerMap[uint(to_user_id)], user)
-			UserFollowerCountMap[uint(to_user_id)]++
-			// 写入数据库
-			user.FollowCount++
-			user.FollowID += "." + strconv.Itoa(to_user_id)
-			UpdateUser(user)
-			follow.FollowerCount++
-			follow.FollowerID += "." + strconv.Itoa(int(user.ID))
-			UpdateUser(follow)
+			c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "Can't focus on yourself"})
 		} else {
-			fmt.Println("已经关注啦！！！！！！")
-			follow.IsFollow = false
-			// 更改缓冲区
-			// 找到
-			del_idx := -1
-			for _, u := range UserFollowMap[user.ID] {
-				del_idx++
-				if u.ID == follow.ID {
-					break
+			//如果没有添加关注
+			if action_type == "1" {
+				sum := 0
+				for _, uer := range UserFollowerMap[follow.ID] {
+					if user.ID == uer.ID {
+						sum++
+					}
 				}
-			}
-			// 从关注列表中移除
-			if del_idx > -1 && del_idx < len(UserFollowMap[user.ID]) {
-				UserFollowMap[user.ID] = append(UserFollowMap[user.ID][:del_idx], UserFollowMap[user.ID][del_idx+1:]...)
-				UserFollowCountMap[user.ID]--
-				// 修改对象
-				uids := strings.Split(user.FollowID, ".")[1:]
-				uids = append(uids[:del_idx], uids[del_idx+1:]...)
-				uids_str := "." + strings.Join(uids, ".")
-				user.FollowID = uids_str
+				if sum == 0 {
+					AddConcern(user, follow)
+					c.JSON(http.StatusOK, Response{StatusCode: 0})
+				} else {
+					c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "Already concerned"})
+				}
+
 			}
 
-			del_idx = -1
-			for _, u := range UserFollowerMap[follow.ID] {
-				del_idx++
-				if u.ID == user.ID {
-					break
-				}
-			}
-			// 从关注列表中移除
-			if -1 < del_idx && del_idx < len(UserFollowerMap[follow.ID]) {
-				UserFollowerMap[follow.ID] = append(UserFollowerMap[follow.ID][:del_idx], UserFollowerMap[follow.ID][del_idx+1:]...)
-				UserFollowerCountMap[follow.ID]--
-				// 修改对象
-				uids := strings.Split(follow.FollowerID, ".")[1:]
-				fmt.Println(uids)
-				uids = append(uids[:del_idx], uids[del_idx+1:]...)
-				uids_str := "." + strings.Join(uids, ".")
-				follow.FollowerID = uids_str
+			if action_type == "2" {
+				CancelConcern(user, follow) //取消关注
+				c.JSON(http.StatusOK, Response{StatusCode: 0})
 			}
 
-			// 写入数据库
-			UpdateUser(user)
-			db.Model(&follow).Update("IsFollow", false)
-			// UpdateUser(follow)
 		}
-		c.JSON(http.StatusOK, Response{StatusCode: 0})
 	} else {
-		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "User doesn't exist"})
+		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "Please login"})
 	}
+}
+
+// 添加关注
+
+func AddConcern(NowUser User, follow User) {
+	follow.IsFollow = true
+	UserFollowMap := GetUserFollowMap("UserFollowMap", Client)
+	UserFollowCountMap := GetUserFollowCountMap("UserFollowCountMap", Client)
+	UserFollowerMap := GetUserFollowerMap("UserFollowerMap", Client)
+	UserFollowerCountMap := GetUserFollowerCountMap("UserFollowerCountMap", Client)
+	// 更改缓冲区
+	UserFollowMap[NowUser.ID] = append(UserFollowMap[NowUser.ID], follow)
+	UserFollowCountMap[NowUser.ID]++
+
+	UserFollowerMap[follow.ID] = append(UserFollowerMap[follow.ID], NowUser)
+	UserFollowerCountMap[follow.ID]++
 	SaveUserFollowMap("UserFollowMap", Client, UserFollowMap)
-	SaveUserFollowerMap("UserFollowerMap", Client, UserFollowerMap)
 	SaveUserFollowCountMap("UserFollowCountMap", Client, UserFollowCountMap)
+	SaveUserFollowerMap("UserFollowerMap", Client, UserFollowerMap)
 	SaveUserFollowerCountMap("UserFollowerCountMap", Client, UserFollowerCountMap)
+	// 写入数据库
+	NowUser.FollowCount = UserFollowCountMap[NowUser.ID]
+
+	if NowUser.FollowID == "." {
+		NowUser.FollowID += strconv.Itoa(int(follow.ID))
+	} else {
+
+		NowUser.FollowID += "." + strconv.Itoa(int(follow.ID))
+	}
+
+	UpdateUser(NowUser)
+
+	follow.FollowerCount = UserFollowerCountMap[follow.ID]
+	if follow.FollowerID == "." {
+		follow.FollowerID += strconv.Itoa(int(NowUser.ID))
+	} else {
+		follow.FollowerID += "." + strconv.Itoa(int(NowUser.ID))
+	}
+	UpdateUser(follow)
+
+}
+
+//取消关注
+
+func CancelConcern(NowUser User, follow User) {
+
+	follow.IsFollow = false
+
+	//从当前登录用户关注列表中移除
+	// 更改缓冲区
+	UserFollowMap := GetUserFollowMap("UserFollowMap", Client)
+	UserFollowCountMap := GetUserFollowCountMap("UserFollowCountMap", Client)
+
+	for index, u := range UserFollowMap[NowUser.ID] {
+		if u.ID == follow.ID {
+			UserFollowMap[NowUser.ID] = append(UserFollowMap[NowUser.ID][:index], UserFollowMap[NowUser.ID][index+1:]...)
+			UserFollowCountMap[NowUser.ID]--
+			//  fmt.Println("当前用户的关注数：", UserFollowCountMap[NowUser.ID])
+			NowUser.FollowCount = UserFollowCountMap[NowUser.ID]
+			uids := strings.Split(NowUser.FollowID, ".")[1:]
+			uids = append(uids[:index], uids[index+1:]...)
+			uids_str := "." + strings.Join(uids, ".")
+			NowUser.FollowID = uids_str
+			break
+		}
+	}
+	// 修改对象
+	SaveUserFollowMap("UserFollowMap", Client, UserFollowMap)
+	SaveUserFollowCountMap("UserFollowCountMap", Client, UserFollowCountMap)
+	// 写入数据库
+	UpdateUser(NowUser)
+
+	//发布视频用户的粉丝列表里删除
+	//修改缓冲区
+	UserFollowerMap := GetUserFollowerMap("UserFollowerMap", Client)
+	UserFollowerCountMap := GetUserFollowerCountMap("UserFollowerCountMap", Client)
+	for index, u := range UserFollowerMap[follow.ID] {
+
+		if u.ID == NowUser.ID {
+			UserFollowerMap[follow.ID] = append(UserFollowerMap[follow.ID][:index], UserFollowerMap[follow.ID][index+1:]...)
+			UserFollowerCountMap[follow.ID]--
+			//	fmt.Println("视频用户的粉丝数：",UserFollowerCountMap[follow.ID])
+
+			follow.FollowerCount = UserFollowerCountMap[follow.ID]
+			uids := strings.Split(follow.FollowerID, ".")[1:]
+			uids = append(uids[:index], uids[index+1:]...)
+			uids_str := "." + strings.Join(uids, ".")
+
+			follow.FollowerID = uids_str
+			break
+		}
+	}
+	// 修改对象
+	SaveUserFollowerMap("UserFollowerMap", Client, UserFollowerMap)
+	SaveUserFollowerCountMap("UserFollowerCountMap", Client, UserFollowerCountMap)
+	//写入数据库
+	UpdateUser(follow)
+
 }
 
 // FollowList all users have same follow list
@@ -157,11 +218,11 @@ func FollowList(c *gin.Context) {
 // FollowerList all users have same follower list
 func FollowerList(c *gin.Context) {
 	user_id, err := strconv.Atoi(c.Query("user_id"))
+	UserFollowerMap := GetUserFollowMap("UserFollowerMap", Client)
 	if err != nil {
 		fmt.Println("Get user_id faild! ", err)
 		c.JSON(http.StatusOK, Response{StatusCode: 1, StatusMsg: "get user_id faile !"})
 	}
-	UserFollowerMap := GetUserFollowerMap("UserFollowerMap", Client)
 	c.JSON(http.StatusOK, UserListResponse{
 		Response: Response{
 			StatusCode: 0,
